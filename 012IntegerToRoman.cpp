e2e08a1970e16f4ebfc631e0b40dd492d5239560
apprentice7 <kangkai9410@gmail.com>
2015-08-16 17:52:02 +0800

    Update 012IntegerToRoman.cpp

--- 012IntegerToRoman.cpp
+++ 012IntegerToRoman.cpp
@@ -14,6 +14,24 @@
 #include <string>
 using namespace std;
 
+//greedy algorithm: is an algorithm that follows the problem solving heuristic of 
+//making the locally optimal choice at each stage with the hope of finding a global optimum.
+
+/*
+string symbol[] =   {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};    
+    int value[]     =   {1000,900,500,400, 100, 90,  50, 40,  10, 9,   5,  4,   1}; 
+    string result;
+
+    for(int i=0; num!=0; i++){
+        while(num >= value[i]){
+            num -= value[i];
+            result+=symbol[i];
+        }
+    }
+
+    return result;
+*/
+
 class Solution{
 public:
 	string intToRoman(int num){
