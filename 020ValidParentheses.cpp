2a41d623992e0f92629b0f99ea954ab9aabe9a40
apprentice7 <kangkai9410@gmail.com>
2015-08-21 22:07:37 +0800

    format changing

--- 020ValidParentheses.cpp
+++ 020ValidParentheses.cpp
@@ -19,29 +19,29 @@ class Solution {
 public:
     bool isValid(string s) {
         if(s.size() % 2 == 1 ) return false;
-		string stack;
-		while(s.size()>0){
-			char Last = s.back();
-			if(Last == '}' || Last == ']' || Last == ')'){
+	string stack;
+	while(s.size()>0){
+		char Last = s.back();
+		if(Last == '}' || Last == ']' || Last == ')'){
+			s.pop_back();
+			stack.push_back(Last);
+		}
+		else if(Last == '{' || Last == '[' || Last == '('){
+			if(stack.empty()) return false;
+			char StackLast = stack.back();
+			if((Last == '{' && StackLast=='}') || (Last == '[' && StackLast==']') || (Last == '(' && StackLast==')')){
 				s.pop_back();
-				stack.push_back(Last);
-			}
-			else if(Last == '{' || Last == '[' || Last == '('){
-				if(stack.empty()) return false;
-				char StackLast = stack.back();
-				if((Last == '{' && StackLast=='}') || (Last == '[' && StackLast==']') || (Last == '(' && StackLast==')')){
-					s.pop_back();
-					stack.pop_back();
-				}
-				else{
-					return false;
-				}	
+				stack.pop_back();
 			}
 			else{
-				s.pop_back();
-			}
+				return false;
+			}	
 		}
-		return(s.size() == 0 && stack.size() == 0);
+		else{
+			s.pop_back();
+		}
+	}
+	return(s.size() == 0 && stack.size() == 0);
     }
 };
 
