ba9f602f42297de8a42bfed4920a68d8f11155ab
apprentice7 <kangkai9410@gmail.com>
2015-08-23 00:30:56 +0800

    Create 022GenerateParentheses.cpp

---
+++ 022GenerateParentheses.cpp
@@ -0,0 +1,58 @@
+// Source : https://oj.leetcode.com/problems/
+// Author : Tony
+// Date   : 23-08-2015
+
+/********************************************************************************** 
+* Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+* For example, given n = 3, a solution set is:
+* "((()))", "(()())", "(())()", "()(())", "()()()"              
+**********************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> output;
+        string out;
+        if(n <= 0) return output;
+        if(n == 1){
+        	out = "()";
+        	output.push_back(out);
+        	return output;
+        } 
+        else{
+        	generate(output, n, n, out);
+        }
+        return output;
+    }
+private: 
+	void generate(vector<string>& v, int left, int right, string s){
+		if(left == 0 && right == 0){
+			v.push_back(s);
+			return;
+		}
+		if(left > 0){
+			generate(v, left-1, right, s+'(');
+		}
+		if(right > 0 && right > left){
+			generate(v, left, right-1, s+')');
+		}
+
+	}
+};
+
+/* //For testing
+int main(){
+	Solution test;
+	vector<string> result;
+    result = test.generateParenthesis(4);
+	for(int t=0; t<result.size();t++){
+		cout << result[t] << endl;
+	}
+	return 0;
+}
+*/
