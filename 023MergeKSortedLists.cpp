bb1dd02d39d18d39cd287597c71d0ad834a172e2
apprentice7 <kangkai9410@gmail.com>
2015-08-23 18:07:45 +0800

    Add method that pass the leetcode test

--- 023MergeKSortedLists.cpp
+++ 023MergeKSortedLists.cpp
@@ -88,6 +88,64 @@ public:
         return output;
     }
 };
+/* 
+//This method meet the time limit requirement.
+//In merge two sorted lists:
+//  It doesn't call other method to init or add node, which is inefficient
+//In merge k sorted lists:
+//  It use some sort of divide and conquer. Merger the original list first
+//  and then merge the merged list.
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode* p1 = new ListNode(0);
+        ListNode* p2 = new ListNode(0);
+        ListNode* MergedList = new ListNode(0);
+        int n = lists.size();
+        if(n<1) return NULL;
+        if(n<2) return lists[0];
+        
+        int size = lists.size();
+        while(size >1){
+            p1 = lists.back();
+            lists.pop_back();
+            p2 = lists.back();
+            lists.pop_back();
+            MergedList = mergeTwoLists(p1, p2);
+            lists.insert(lists.begin(), MergedList);
+            size = lists.size();
+        }
+        return lists[0];
+    }
+
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *p1 = l1,  *p2=l2;
+        static ListNode dummy(0);
+
+        dummy.next = p1;
+        ListNode *prev = &dummy;
+
+        while(p1 && p2){
+            if(p1->val < p2->val){
+                prev = p1;
+                p1 = p1->next;
+             }else{
+                prev->next = p2;
+                p2 = p2->next;
+                prev = prev->next;
+                prev->next = p1;
+            }
+        }
+        if (p2){
+            prev->next = p2;
+        }
+
+        return dummy.next;
+    }
+};
+*/
+
 
 /* //For testing
 int main(){
