c98b1154e29478d2be9ac9ede5a6906ea20f482a
apprentice7 <kangkai9410@gmail.com>
2015-09-12 01:04:58 +0800

    Update 039combinationSum.cpp

--- 039combinationSum.cpp
+++ 039combinationSum.cpp
@@ -2,7 +2,19 @@
 // Author : Tony
 // Date   : 11-09-2015
 
-
+/********************************************************************************** 
+* Given a set of candidate numbers (C) and a target number (T), find all unique combinations 
+* in C where the candidate numbers sums to T.
+* The same repeated number may be chosen from C unlimited number of times.
+* Note:
+* All numbers (including target) will be positive integers.
+* Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+* The solution set must not contain duplicate combinations
+* For example, given candidate set 2,3,6,7 and target 7, 
+* A solution set is: 
+* [7] 
+* [2, 2, 3]              
+**********************************************************************************/
 
 #include <iostream>
 #include <vector>
@@ -11,38 +23,47 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int> > combinationSum(vector<int>& candidates, int target) {
         vector <vector<int> > output;
 		sort(candidates.begin(),candidates.end());
-		if(candidates[0] > target) return output;
+		if(candidates[0] > target || candidates.size() < 1) return output;
 
 		vector<int> temp;
-		findCombination(output, temp, candidates, target);
+		findCombination(output, temp, candidates, target, 0);
+		return output;
     }
 
-	void findCombination(vector< vector<int> >& output, vector<int> temp, vector<int>& candidates, int target){
-		for(int i=0; i<candidates.size(); i++){
-			if(candidates[i] < target){
-				target = target - candidates[i];
-				temp.push_back(candidates[i]);
-				findCombination(output, temp, candidates, target);
-			}
-			else if(candidates[i] > target){
-				break;
-			}
-			else if(candidates[i] == target){
-				temp.push_back(candidates[i]);
-				output.push_back(temp);
-				return;
-			}
+	void findCombination(vector< vector<int> >& output, vector<int>& temp, vector<int>& candidates, int target, int pos){
+		if(target < 0){
+			return;
 		}
-		return;
+		if(target == 0){
+			output.push_back(temp);
+			return;
+		}
+		for(int i=pos; i<candidates.size(); i++){
+			if(i>pos && candidates[i] == candidates[i-1]) continue;
+			temp.push_back(candidates[i]);
+			findCombination(output,temp,candidates,target - candidates[i],i);
+			temp.pop_back();
+		}
 	}
 };
- //For testing
+/* //For testing
 int main(){
 	Solution test;
-	
+	vector<int> candidates;
+	int arr[] = {2,3,6,7};
+	candidates.assign(arr,arr+4);
+	vector< vector<int> > result;
+	result = test.combinationSum(candidates, 7);
+	for(int i=0; i< result.size();i++){
+		for(int j=0; j< result[i].size(); j++){
+			cout << result[i][j] << " ";
+		}
+		cout << endl;
+	}
 
 	return 0;
 }
+*/
