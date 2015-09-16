1f7b549cfc17acbbc6122aa8e4e868fedc928399
apprentice7 <kangkai9410@gmail.com>
2015-09-17 00:17:53 +0800

    Update 042trappingRainWater.cpp

--- 042trappingRainWater.cpp
+++ 042trappingRainWater.cpp
@@ -1,6 +1,6 @@
 // Source : https://oj.leetcode.com/problems/
 // Author : Tony
-// Date   : 15-09-2015
+// Date   : 17-09-2015
 
 /********************************************************************************** 
 * Given n non-negative integers representing an elevation map where the width of each bar is 1, 
@@ -21,6 +21,13 @@
 * 6 units of rain water (blue section) are being trapped. Thanks Marcos for contributing this image!
 **********************************************************************************/
 
+/*
+1. go through from left to right:
+	add all water height that right is higher or equal to left
+	and make the lower water equals to left
+2. go through from right to left:
+	add all water height that left is higher than right
+*/
 
 #include <iostream>
 #include <vector>
@@ -36,20 +43,20 @@ public:
         int right=0;
 
         for(int i=0; i<n; i++){
-        	//cout << "start: " << i << endl;
         	left = height[i];
         	int j=i+1;
         	int flag=0;
         	while(j<n){
         		if(height[j] >= left){
         			flag = 1;
-        			break;	
+        			break;
         		}
         		j++;
         	}
         	if(flag == 1 && (j-i)>1 ){
         		for(int k=i+1; k<j; k++){
         			area = area + left - height[k];
+        			height[k] = left;
         		}
         		i = j-1;
         	}
@@ -60,20 +67,20 @@ public:
 			int j = i-1;
 			int flag = 0;
 			while(j>=0){
-				if(height[i] > right){
+				if(height[j] > right ){
 					flag=1;
 					break;
 				}
 				j--;
 			}
 			if(flag==1 && (i-j)>1 ) {
+				//cout <<"baba"<<endl;
 				for(int k=i-1; k>j; k--){
 					area = area + right - height[k];
 				}
 				i = j+1;
 			}
 		}
-
         return area;
     }
 };
@@ -81,9 +88,9 @@ public:
 
 int main(){
 	Solution test;
-	int arr[] = {0,1,0,2,1,0,1,3,2,1,2,1};
+	int arr[] = {5,2,1,2,1,5};
 	vector<int> v;
-	v.assign(arr,arr+12);
+	v.assign(arr,arr+6);
 	int k = test.trap(v);
 	cout << "Result:" << k << endl;
 	return 0;
