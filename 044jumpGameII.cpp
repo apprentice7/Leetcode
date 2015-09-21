f95d0c83e2de3d51eec880581712bf5ef3a1bae8
apprentice7 <kangkai9410@gmail.com>
2015-09-21 18:08:38 +0800

    Update 044jumpGameII.cpp

--- 044jumpGameII.cpp
+++ 044jumpGameII.cpp
@@ -1,8 +1,43 @@
+// Source : https://oj.leetcode.com/problems/jump-game-ii/
+// Author : Hao Chen
+// Date   : 2014-07-18
+
+/********************************************************************************** 
+* Given an array of non-negative integers, you are initially positioned at the first index of the array.
+* Each element in the array represents your maximum jump length at that position. 
+* Your goal is to reach the last index in the minimum number of jumps.
+* For example:
+* Given array A = [2,3,1,1,4]
+* The minimum number of jumps to reach the last index is 2. 
+* (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
+**********************************************************************************/
+
 #include <iostream>
-#include <string>
 #include <vector>
+#include <math.h>
 using namespace std;
 
+class Solution{
+public:
+	int jump(vector<int>& nums){
+		int n = nums.size();
+		int ret = 0;
+		int last = 0;
+		int curr = 0;
+
+		for(int i=0; i<n; i++){
+			if(i>last){
+				if(last == curr && last < n-1) return -1;
+				last = curr;
+				++ret;
+			}
+			curr = max(curr,i+nums[i]);
+		}
+		return ret;
+	}
+};
+
+/* // cannot pass time limit
 class Solution {
 public:
     int jump(vector<int>& nums) {
@@ -36,14 +71,15 @@ public:
 		}
 	}
 };
-
- //For testing
+*/
+/* //For testing
 int main(){
 	Solution test;
-	int arr[] = {1,1,1,1,1};
+	int arr[] = {2,3,1,1,0,4};
 	vector<int> v;
-	v.assign(arr,arr+5);
+	v.assign(arr,arr+6);
 	cout << test.jump(v) << endl;
 
 	return 0;
 }
+*/
