05ace8848898b40c6649b0125c973e42a51ca8b7
apprentice7 <kangkai9410@gmail.com>
2015-09-22 15:58:41 +0800

    Update 046permutations.cpp

--- 046permutations.cpp
+++ 046permutations.cpp
@@ -2,14 +2,11 @@
 // Author : Tony
 // Date   : 22-09-2015
 
-/********************************************************************************** 
-* Given an array of non-negative integers, you are initially positioned at the first index of the array.
-* Each element in the array represents your maximum jump length at that position. 
-* Your goal is to reach the last index in the minimum number of jumps.
-* For example:
-* Given array A = [2,3,1,1,4]
-* The minimum number of jumps to reach the last index is 2. 
-* (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
+/**********************************************************************************
+* Given a collection of numbers, return all possible permutations.
+* For example,
+* [1,2,3] have the following permutations:
+* [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
 **********************************************************************************/
 
 #include <iostream>
